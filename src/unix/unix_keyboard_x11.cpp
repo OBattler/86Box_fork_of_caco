1c8cd459e03d4eb1a2107ff0a2efa9c4365c311b
Cacodemon345 <wahil1976@outlook.com>
2021-10-03 16:50:06 +0600

    Add Xvnc keymap

--- src/unix/unix_keyboard_x11.cpp
+++ src/unix/unix_keyboard_x11.cpp
@@ -369,6 +369,88 @@ std::array<uint32_t, 256> x11_to_xt_2
     0x153
 };
 
+std::array<uint32_t, 256> x11_to_xt_vnc
+{
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0x1D,
+    0x11D,
+    0x2A,
+    0x36,
+    0,
+    0,
+    0x38,
+    0x138,
+    0x39,
+    0x0B,
+    0x02,
+    0x03,
+    0x04,
+    0x05,
+    0x06,
+    0x07,
+    0x08,
+    0x09,
+    0x0A,
+    0x0C,
+    0x0D,
+    0x1A,
+    0x1B,
+    0x27,
+    0x28,
+    0x29,
+    0x33,
+    0x34,
+    0x35,
+    0x2B,
+    0x1E,
+    0x30,
+    0x2E,
+    0x20,
+    0x12,
+    0x21,
+    0x22,
+    0x23,
+    0x17,
+    0x24,
+    0x25,
+    0x26,
+    0x32,
+    0x31,
+    0x18,
+    0x19,
+    0x10,
+    0x13,
+    0x1F,
+    0x14,
+    0x16,
+    0x2F,
+    0x11,
+    0x2D,
+    0x15,
+    0x2C,
+    0x0E,
+    0x1C,
+    0x0F,
+    0x01,
+    0x153,
+    0x147,
+    0x14F,
+    0x149,
+    0x151,
+    0x148,
+    0x150,
+    0x14B,
+    0x14D,
+};
+
 static Display* x11display;
 static std::array<uint32_t, 256>& selected_keycode = x11_to_xt_base;
 uint16_t x11_keycode_to_keysym(uint32_t keycode)
@@ -379,6 +461,10 @@ uint16_t x11_keycode_to_keysym(uint32_t keycode)
         if (XKeysymToKeycode(x11display, XK_Home) == 110)
         {
             selected_keycode = x11_to_xt_2;
+        }
+        else if (XKeysymToKeycode(x11display, XK_Home) == 69)
+        {
+            selected_keycode = x11_to_xt_vnc;
         }
     }
     return selected_keycode[keycode];
